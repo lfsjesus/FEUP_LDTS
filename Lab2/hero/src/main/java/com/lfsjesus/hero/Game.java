ef1c4b035db342384f8016986586624a1204b26b
Luís Jesus <lfsjesus@outlook.com>
2022-09-27 16:23:24 +0100

    Added movement by keystroke

--- Lab2/hero/src/main/java/com/lfsjesus/hero/Game.java
+++ Lab2/hero/src/main/java/com/lfsjesus/hero/Game.java
@@ -1,23 +1,69 @@
 package com.lfsjesus.hero;
 
 import com.googlecode.lanterna.TerminalSize;
+import com.googlecode.lanterna.TextCharacter;
+import com.googlecode.lanterna.input.KeyStroke;
+import com.googlecode.lanterna.input.KeyType;
 import com.googlecode.lanterna.screen.Screen;
 import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
 import com.googlecode.lanterna.terminal.Terminal;
 
+import java.io.IOException;
+
 public class Game {
-   Game() {
-       TerminalSize terminalSize = new TerminalSize(int column, int row);
+    private Screen screen;
+    private int x = 10;
+    private int y = 10;
+
+   public Game(int col, int row) throws IOException {
+       TerminalSize terminalSize = new TerminalSize(col, row);
        DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory().setInitialTerminalSize(terminalSize);
        Terminal terminal = terminalFactory.createTerminal();
-       Screen screen = new TerminalScreen(terminal);
+       screen = new TerminalScreen(terminal);
        screen.setCursorPosition(null); // we don't need a cursor
        screen.startScreen(); // screens must be started
        screen.doResizeIfNecessary(); // resize screen if necessary
-       screen.clear();
-       screen.setCharacter(int column, int row, TextCharacter.fromCharacter('X')[0]);
-       screen.refresh();
+
     }
+
+    public void draw() throws IOException {
+        screen.clear();
+        screen.setCharacter(x, y, TextCharacter.fromCharacter('X')[0]);
+        screen.refresh();
+    }
+
+    public void run() throws IOException {
+       while (true) {
+           draw();
+           KeyStroke key = screen.readInput();
+           if (key.getKeyType() == KeyType.Character && key.getCharacter() == 'q') {
+                screen.close();
+           }
+           if (key.getKeyType() == KeyType.EOF) {
+               break;
+           }
+           processKey(key);
+       }
+    }
+
+    private void processKey(KeyStroke key) {
+        switch(key.getKeyType()) {
+            case ArrowUp:
+                y -= 1;
+                break;
+            case ArrowDown:
+                y += 1;
+                break;
+            case ArrowLeft:
+                x -= 1;
+                break;
+            case ArrowRight:
+                x += 1;
+                break;
+        }
+
+    }
+
 
 }
--- Lab2/hero/src/main/java/com/lfsjesus/hero/Main.java
+++
@@ -1,5 +0,0 @@
-package com.lfsjesus.hero;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
