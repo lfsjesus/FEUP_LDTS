d4e119e25d31b84a5a2e590d744fa2ea5ebe60ab
Luís Jesus <lfsjesus@outlook.com>
2022-10-11 09:53:37 +0100

    Added the first stub

--- Lab3/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ Lab3/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -43,9 +43,15 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
+        //List<Integer> list = Arrays.asList(1,2,4,2,5);
+        class StubListDeduplicator implements GenericListDeduplicator{
+            @Override public List<Integer> deduplicate(List<Integer> list) {
+                return  Arrays.asList(1, 2, 4, 5);
+            }
+        }
         ListAggregator aggregator = new ListAggregator();
-        int distinct = aggregator.distinct(list);
-
+        StubListDeduplicator deduplicator = new StubListDeduplicator();
+        int distinct = aggregator.distinct(list, deduplicator);
         Assertions.assertEquals(4, distinct);
     }
 
@@ -56,5 +62,20 @@ public class ListAggregatorTest {
 
         Assertions.assertEquals(-1, max);
     }
+
+    @Test
+    public void distinctbug_8726() {
+        List<Integer> list = Arrays.asList(1,2,4,2);
+        class StubListDeduplicator implements GenericListDeduplicator{
+            @Override public List<Integer> deduplicate(List<Integer> list) {
+                return  Arrays.asList(1, 2, 4);
+            }
+        }
+        ListAggregator aggregator = new ListAggregator();
+        StubListDeduplicator deduplicator = new StubListDeduplicator();
+        int distinct = aggregator.distinct(list, deduplicator);
+        Assertions.assertEquals(3, distinct);
+    }
+
 
 }
