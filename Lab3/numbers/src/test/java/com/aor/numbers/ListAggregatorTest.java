d226497dfde8332afd2744ade05e81b161e7ee05
Luís Jesus <lfsjesus@outlook.com>
2022-10-11 09:05:01 +0100

    Add @BeforeEach helper method

--- Lab3/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
+++ Lab3/numbers/src/test/java/com/aor/numbers/ListAggregatorTest.java
@@ -1,16 +1,22 @@
 package com.aor.numbers;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class ListAggregatorTest {
+    private List<Integer> list;
+
+    @BeforeEach
+    public void setup() {
+        list = Arrays.asList(1,2,4,2,5);
+    }
+
     @Test
     public void sum() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int sum = aggregator.sum(list);
 
@@ -19,8 +25,6 @@ public class ListAggregatorTest {
 
     @Test
     public void max() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int max = aggregator.max(list);
 
@@ -39,8 +43,6 @@ public class ListAggregatorTest {
 
     @Test
     public void distinct() {
-        List<Integer> list = Arrays.asList(1,2,4,2,5);
-
         ListAggregator aggregator = new ListAggregator();
         int distinct = aggregator.distinct(list);
 
