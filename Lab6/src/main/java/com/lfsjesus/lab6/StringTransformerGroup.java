0d6692e10645f5a3ae96afd37b1eb5daee3bd08f
Luís Jesus <luisjesus__2003@outlook.com>
2022-11-08 10:24:43 +0000

    added transformergroup

---
+++ Lab6/src/main/java/com/lfsjesus/lab6/StringTransformerGroup.java
@@ -0,0 +1,18 @@
+package com.lfsjesus.lab6;
+
+import java.util.List;
+
+public class StringTransformerGroup {
+    private List<StringTransformer> transformers;
+
+    public StringTransformerGroup(List<StringTransformer> transformers) {
+        this.transformers = transformers;
+    }
+
+    public void execute(StringDrink drink) {
+        for (StringTransformer s : transformers) {
+            s.execute(drink);
+        }
+    }
+
+}
--- Lab6/src/test/java/StringTransformerTest.java
+++ Lab6/src/test/java/StringTransformerTest.java
@@ -53,4 +53,17 @@ public class StringTransformerTest {
         recipe.mix(drink);
         assertEquals("dCbX-DcBa", drink.getText());
     }
+
+    @Test
+    public void transformerGroup() {
+        StringDrink drink = new StringDrink( "AbCd-aBcD");
+        StringInverter si = new StringInverter();
+        StringCaseChanger cc = new StringCaseChanger();
+        List<StringTransformer> transformers = new ArrayList<>();
+        transformers.add(si);
+        transformers.add(cc);
+        StringTransformerGroup tg = new StringTransformerGroup(transformers);
+        tg.execute(drink);
+        assertEquals("dCbA-DcBa", drink.getText());
+    }
 }
